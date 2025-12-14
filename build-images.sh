de2f60661a4f4a3114aca2198e6a95167ffef836
Martin Bhuong <martin@genius.ke>
2025-12-14 18:14:53 +0300

    Update image label to version 2.5.0-beta20251125

--- build-images.sh
+++ build-images.sh
@@ -45,7 +45,7 @@ buildah config --entrypoint=/ \
     --label="org.nethserver.authorizations=traefik@node:routeadm cluster:accountconsumer" \
     --label="org.nethserver.tcp-ports-demand=1" \
     --label="org.nethserver.rootfull=0" \
-    --label="org.nethserver.images=docker.io/openbao/openbao:2.4" \
+    --label="org.nethserver.images=docker.io/openbao/openbao:2.5.0-beta20251125" \
     "${container}"
 # Commit the image
 buildah commit "${container}" "${repobase}/${reponame}"
